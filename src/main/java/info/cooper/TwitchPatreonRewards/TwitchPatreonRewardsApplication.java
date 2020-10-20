46c36f686246e9b35266e39b1997248019751a06
Henry Cooper <cooper.henry30@googlemail.com>
2020-10-20 21:48:51 +0100

    spring boot initialized

---
+++ src/main/java/info/cooper/TwitchPatreonRewards/TwitchPatreonRewardsApplication.java
@@ -0,0 +1,13 @@
+package info.cooper.TwitchPatreonRewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchPatreonRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchPatreonRewardsApplication.class, args);
+	}
+
+}
