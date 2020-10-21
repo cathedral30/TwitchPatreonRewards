bc04c3ae4687a86123181eec856cbb518955cf4f
Henry Cooper <cooper.henry30@googlemail.com>
2020-10-21 22:37:31 +0100

    handling oauth initial response

--- src/main/java/info/cooper/TwitchPatreonRewards/controllers/HomeController.java
+++ src/main/java/info/cooper/TwitchPatreonRewards/controllers/HomeController.java
@@ -3,13 +3,17 @@ package info.cooper.TwitchPatreonRewards.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(method= RequestMethod.GET)
-    public String getHome() {
+    @RequestMapping(method=RequestMethod.GET)
+    public String getHome(@RequestParam(value="code", required = false) String code, @RequestParam(value="state", required = false) String state) {
+        if (code != null) {
+            System.out.println(code);
+            System.out.println(state);
+        }
         return "index";
     }
-
 }
